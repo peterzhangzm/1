4719e4f106329e78877b72022876353c3d0b08ed
peter <peterzhang024680@gmail.com>
2022-11-08 00:29:29 +0800

    典型的简单的UVM平台

---
+++ my_env.sv
@@ -0,0 +1,15 @@
+class my_env extends uvm_env;
+    `uvm_component_utils(my_env)
+
+    master_agent m_agent;
+
+    function new(string name = "", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        m_agent = master_agent::type_id::create("m_agent", this);
+    endfunction
+
+endclass
---
+++ my_monitor.sv
@@ -0,0 +1,14 @@
+class my_monitor extends uvm_monitor;
+    `uvm_component_utils(my_monitor)
+
+    function new (string name = "", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    virtual task run_phase(uvm_phase phase);
+        forever begin
+            `uvm_info("MON_RUN_PHASE", "Monitor run!", UVM_MEDIUM)
+            #100;
+        end
+    endtask
+endclass
